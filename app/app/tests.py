6dd2b03fdae16b7b860b94b7315ef00f4791690b
hbayernetw <ha.mi.bayer@gmail.com>
2023-03-24 12:42:49 +0100

    Database added

---
+++ app/app/tests.py
@@ -0,0 +1,22 @@
+"""
+Sample Test
+"""
+from django.test import SimpleTestCase
+
+from app import calc
+
+class calcTests(SimpleTestCase):
+    """Test the calc mocule."""
+
+    def test_add_numbers(self):
+        """Tests adding numbers together"""
+        res = calc.add(5, 6)
+
+        self.assertEqual(res, 11)
+
+    def test_subtract_numbers(self):
+        """Test subtracting numbers"""
+        res = calc.subtract(5, 6)
+
+        self.assertEqual(res, -1)
+
